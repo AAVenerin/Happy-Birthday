cffae2c7df1d8f85f037b6e47eb5167a9cdcfa6d
Venerin <venerin.andrey@gmail.com>
2022-08-14 15:16:21 +0300

    Congratulations are written!

---
+++ TEMA.py
@@ -0,0 +1,10 @@
+def congratulate(age, wish):
+    return f'Hi Tema! \nYou are {age} years old today! \nI wish you {wish}!\
+     \nHugs and handshake! \nFaithfully yours \nAndrey'
+# Я пока не знаю, как писать красивый и читабельный код, поэтому приношу
+# глубочайшие извинения за всю ту боль, которую испытает нормальный
+# программист при чтении такого "чуда" :D
+# Я старался со всех сил! 
+print(congratulate\
+    (input('Enter your actual age: '),
+                input('Write down your the most desirable wish: ')))
